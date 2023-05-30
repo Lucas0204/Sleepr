08ad0803fdf3b790ca560d4bf914e50397c38640
Lucas0204 <lucaszordan0204@gmail.com>
2023-05-30 20:23:36 -0300

    adding jwt authentication and get user route using cookie

--- apps/auth/src/users/users.service.ts
+++ apps/auth/src/users/users.service.ts
@@ -1,18 +1,32 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable, UnauthorizedException, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UsersRepository } from './users.repository';
 import * as bcryptjs from 'bcryptjs';
 import { UserDocument } from './models/user.schema';
+import { GetUserDto } from './dto/get-user.dto';
 
 @Injectable()
 export class UsersService {
     constructor(private readonly usersRepository: UsersRepository) {}
 
     async create(createUserDto: CreateUserDto) {
+        await this.validateCreateUserDto(createUserDto);
+
         return this.usersRepository.create({
             ...createUserDto,
             password: await bcryptjs.hash(createUserDto.password, 10)
         });
+    }
+
+    private async validateCreateUserDto({ email }: CreateUserDto) {
+        try {
+            await this.usersRepository.findOne({ email });
+        } catch (err) {
+            return;
+        }
+
+        // user email already exists
+        throw new UnprocessableEntityException('Invalid data to create user!');
     }
 
     async verifyUser(email: string, password: string): Promise<UserDocument> {
@@ -24,5 +38,9 @@ export class UsersService {
         }
 
         return user;
+    }
+
+    getUser(getUserDto: GetUserDto): Promise<UserDocument> {
+        return this.usersRepository.findOne(getUserDto);
     }
 }
